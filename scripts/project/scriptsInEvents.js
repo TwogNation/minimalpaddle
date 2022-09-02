a70c8d8ed2f6995bb988e4f37470d8a728cc7159
Matheus N <matheusnaschaves@gmail.com>
2022-09-02 20:19:08 +0200

    da

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,20 @@
+var postMsg = ''
+
+function postText(value){
+	console.log('iFrame sender: ' + value) 
+	parent.postMessage(value, "*")
+}
+
+
+
+const scriptsInEvents = {
+
+		async Game_Event13_Act4(runtime, localVars)
+		{
+			postText(runtime.globalVars.finalScore)
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
